047b74f3ddba18b98ff49e7d57d4291cb52cef0a
Tim Barrass <tim.barrass@gmail.com>
2011-08-03 17:43:25 +0100

    fleshed out example program to process a file and display the results

--- HistogramTool/Program.cs
+++ HistogramTool/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace HistogramTool
+﻿using System;
+
+namespace HistogramTool
 {
     class Program
     {
@@ -8,9 +10,19 @@
             var rule = new LinearBucketingRule(10d);
             var histo = new Histogram(rule);
 
-            var data = loader.LoadSingleValuedFile("basicHistoData.txt");
+            var data = loader.LoadSingleValuedFile(args[0]);
 
             histo.Build(data);
+
+            Display(rule, histo);
+        }
+
+        private static void Display(LinearBucketingRule rule, Histogram histogram)
+        {
+            for(int i = 0; i < histogram.Buckets.Length; i++)
+            {
+                Console.WriteLine(rule.DetermineValue(i) + "\t" + histogram.Buckets[i]);
+            }
         }
     }
 }
